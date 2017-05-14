c708e583ec1f67c9d444b581322a5597a3ff4ae2
Henning Mosand Stephansen <henningmosand@outlook.com>
2017-05-14 18:11:31 +0200

    First commit

---
+++ TestRuterFluent/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.UI.Animations;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestRuterFluent
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.ButtonForegroundColor = Colors.White;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var accessStatus = await Geolocator.RequestAccessAsync();
+
+            if (accessStatus == GeolocationAccessStatus.Allowed)
+            {
+                var geolocator = new Geolocator() {DesiredAccuracy = PositionAccuracy.High};
+                var location = await geolocator.GetGeopositionAsync();
+                FindName("PositionMapControl");
+                PositionMapControl.MapElements.Add(new MapIcon(){Location = location.Coordinate.Point});
+                await PositionMapControl.TrySetViewAsync(location.Coordinate.Point, 17, null, null, MapAnimationKind.None);
+
+                PositionMapControl.Fade(0.3f, 700, 1500).Start();
+            }
+        }
+
+        private void PlaceTextBox_OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                e.Handled = true;
+
+                ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("textbox", PlaceTextBox);
+                Frame.Navigate(typeof(ShowTripsPage), PlaceTextBox.Text);
+            }
+        }
+    }
+}
---
+++ TestRuterFluent/ShowTripsPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestRuterFluent
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShowTripsPage : Page
+    {
+        public ShowTripsPage()
+        {
+            this.InitializeComponent();
+
+            TripListView.ItemsSource = new List<string> {"Stortinget 1 min", "Natonal 2 min", "Scooby doo 5 min"};
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var place = e.Parameter as string;
+
+            PlaceTextBox.Text = place;
+
+            var placeAnimation = ConnectedAnimationService.GetForCurrentView().GetAnimation("textbox");
+
+            placeAnimation?.TryStart(PlaceTextBox, new []{TripListView});
+        }
+    }
+}
